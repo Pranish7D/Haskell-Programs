0fb96dadca633d0ae26f998ef2eca388a1444b13
Pranish K <128279506+Pranish7D@users.noreply.github.com>
2025-03-28 10:26:24 +0530

    Create map-filter-fold.hs

---
+++ Lab6/map-filter-fold.hs
@@ -0,0 +1,5 @@
+productDoublesLessThan10 :: [Int] -> Int
+productDoublesLessThan10 = foldl (*) 1 . map (82) . filter (<= 10)
+
+main :: IO ()
+main = print (productDoublesLessThan10 [5, 12, 9, 20, 15])
